adc187811fe3419a8ecf5cc0b6113550ef251c84
AAlchi <alialchi07@gmail.com>
2025-01-23 16:28:59 -0600

    Initial Commit

---
+++ D214_Trainer/D214_TrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  D214_TrainerApp.swift
+//  D214_Trainer
+//
+//  Created by Ali AI on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct D214_TrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ D214_TrainerTests/D214_TrainerTests.swift
@@ -0,0 +1,17 @@
+//
+//  D214_TrainerTests.swift
+//  D214_TrainerTests
+//
+//  Created by Ali AI on 1/23/25.
+//
+
+import Testing
+@testable import D214_Trainer
+
+struct D214_TrainerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
