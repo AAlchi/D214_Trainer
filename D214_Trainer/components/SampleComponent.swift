8fcf7ff8884bda6775cc3e3b033b4ef6ac1745f5
Ali Alchikh Ibrahim <alialchikhibrahim@Alis-MacBook-Pro.local>
2025-02-15 15:48:13 -0600

    Quick file changes

---
+++ D214_Trainer/components/SampleComponent.swift
@@ -0,0 +1,21 @@
+//
+//  SampleComponent.swift
+//  D214_Trainer
+//
+//  Created by Ali AI on 2/15/25.
+//
+
+import SwiftUI
+
+struct SampleComponent: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SampleComponent()
+}
---
+++ D214_Trainer/pages/SamplePage.swift
@@ -0,0 +1,21 @@
+//
+//  SamplePage.swift
+//  D214_Trainer
+//
+//  Created by Ali AI on 2/15/25.
+//
+
+import SwiftUI
+
+struct SamplePage: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    SamplePage()
+}
