397c03a6943ba92bf7a3385799d39e46207e9a00
Logan Hammond <loganhammond493@gmail.com>
2025-03-13 18:20:12 -0500

    added recipeComponent code

--- D214_Trainer/ContentView.swift
+++ D214_Trainer/ContentView.swift
@@ -1,24 +1,124 @@
 //
 //  ContentView.swift
-//  D214_Trainer
+//  recipeComponent
 //
-//  Created by Ali AI on 1/23/25.
+//  Created by Logan Hammond on 3/11/25.
+//
+
+//
+//  ContentView.swift
+//  appDisplay
+//
+//  Created by Logan Hammond on 2/25/25.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.white.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                   
+                    
+                    Text("D214 Trainer")
+                        .font(.title)
+                        .bold()
+                        .foregroundColor(.white)
+                       
+                    Spacer()
+                    
+                    Text("Recipe | Make a Cappuccino")
+                        .foregroundColor(.white)
+                        .bold()
+                        .font(.largeTitle)
+                        .padding(.top, 30)
+                }
+                .padding()
+                ZStack {
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color.white)
+                        .padding()
+                    
+                    VStack {
+                        HStack {
+                            Text("Cappuccino")
+                                .font(.largeTitle)
+                                .font(.title2)
+                                .bold()
+                                .padding()
+                                .frame(width: 900, height: 100)
+                                .border(Color.black, width: 5)               .background(RoundedRectangle(cornerRadius: 10).fill(Color.red))
+                                .foregroundColor(.white)
+                                .padding()
+                            Spacer()
+                            
+                            Image("cup")
+                                .resizable()
+                                .frame(width: 150, height: 150)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                                .padding()
+                        }
+                        .padding(.horizontal)
+                        
+                        VStack {
+                            
+                            Text("Ingredients")
+                                .font(.headline)
+                                
+                                .padding(.top)
+                                .font(.largeTitle)
+                            Text("1 shot of Espresso")
+                                .padding(.top, 2)
+                                .font(.largeTitle)
+                            Text("150 ml semi skimmed milk")
+                                .padding(.bottom)
+                                .font(.largeTitle)
+                        }
+                        .frame(width: 1000, height: 600)
+                        .border(Color.black, width: 7)
+                        
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.red))
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                        
+                        Spacer()
+                        
+                        HStack {
+                            Spacer()
+                            Button(action: {
+                            }) {
+                                Image(systemName: "arrow.right")
+                                    .padding()
+                                    .frame(width: 100, height: 30)
+                                    .border(Color.black, width: 2)
+                                    .background(RoundedRectangle(cornerRadius: 10).fill(Color.red))
+                                    .padding(.bottom)
+
+                            }
+                            .padding(.trailing, 100)
+                            .padding()
+                        }
+                    }
+                }
+                HStack {
+                    Spacer()
+                    Text("Soft Skill")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .padding(.trailing)
+                }
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct DisplayPage_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
